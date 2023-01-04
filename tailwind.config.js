45324358c4af6ede21271a5e9365d55c31111cb5
dimasrzkm <dimas1.mahendra@gmail.com>
2023-01-04 09:31:44 +0700

    make ui twitter embeded challenge

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      card: ["Source Sans Pro", "sans-serif"],
+    },
   },
   plugins: [],
 };
